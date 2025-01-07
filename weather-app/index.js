853fd23937e5ca3bd743bd7583aa96fab3b86381
chethan7032 <chethanreddy118@gmail.com>
2025-01-07 12:36:30 +0530

    weather-app-but-no-js

---
+++ weather-app/index.js
@@ -0,0 +1,14 @@
+// const apiKey = "d4576306318ac7b4479f58dd4f7ccd8e";
+
+// const weatherDataElement = document.querySelector(".weather-data");
+
+// const cityInputEle = document.querySelector(".city-input");
+
+// const formEl = document.querySelector("form");
+
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(event);
+//   const cityValue = cityInputEle.value;
+//   console.log(cityValue);
+// });
